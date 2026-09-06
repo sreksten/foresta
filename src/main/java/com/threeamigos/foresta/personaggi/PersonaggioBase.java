8f65b1b85b978fcc005e1ec116817f790fb2e720
Stefano Reksten <stefano.reksten@gmail.com>
2026-09-06 14:10:47 +0200

    Piccolo bugfixing per descrizione personaggio

--- src/main/java/com/threeamigos/foresta/personaggi/PersonaggioBase.java
+++ src/main/java/com/threeamigos/foresta/personaggi/PersonaggioBase.java
@@ -283,7 +283,7 @@ public abstract class PersonaggioBase implements Personaggio {
 		} else if (salute < 400) {
 			sb.append("è davvero in salute");
 		} else {
-			sb.append("è sano come un pesce");
+			sb.append("è san").append(getLetteraFinaleAttributo()).append(" come un pesce");
 		}
 		sb.append(", ");
 		int coraggio = getCoraggio();
