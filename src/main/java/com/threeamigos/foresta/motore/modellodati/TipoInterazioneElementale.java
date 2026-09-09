e7f9d512e2fcc92d40f979b475c151a1c291e074
Stefano Reksten <stefano.reksten@gmail.com>
2026-09-09 23:44:16 +0200

    Aggiunte interazioni SUPERCONDUZIONE e ALIMENTAZIONE_FIAMMA

--- src/main/java/com/threeamigos/foresta/motore/modellodati/TipoInterazioneElementale.java
+++ src/main/java/com/threeamigos/foresta/motore/modellodati/TipoInterazioneElementale.java
@@ -7,24 +7,39 @@ package com.threeamigos.foresta.motore.modellodati;
 public enum TipoInterazioneElementale {
 
     // Interazioni con BAGNATO
+    // + FULMINE
     ELETTROCUZIONE,
+    // + GELO
     CONGELAMENTO,
+    // + FUOCO
     VAPORIZZAZIONE,
 
     // Interazioni con BRUCIATO
+    // + ARIA
+    ALIMENTAZIONE_FIAMMA,
+    // + ACQUA
     ESTINZIONE,
+    // + GELO
     SCIOGLIMENTO_TERMICO,
+    // + VELENO
     ESPLOSIONE_DI_GAS,
 
     // Interazioni con CONGELATO
+    // + CONTUNDENTE
     FRANTUMAZIONE_DEL_GHIACCO,
+    // + FUOCO
     DISGELO_VIOLENTO,
+    // + FULMINE
+    SUPERCONDUZIONE,
 
     // Interazioni con MALEDETTO
+    // + NECROTICO
     MIETITURA,
+    // + SACRO
     RIGETTO,
 
     // Interazioni con INFETTATO
+    // + SACRO
     PURIFICAZIONE
 
 }
