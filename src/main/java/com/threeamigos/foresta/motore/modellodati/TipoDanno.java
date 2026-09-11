349f1c126c55c9be642bdf6c5312ac43d2286825
Stefano Reksten <stefano.reksten@gmail.com>
2026-09-11 04:42:05 +0200

    Aggiunto il nome al TipoDanno

--- src/main/java/com/threeamigos/foresta/motore/modellodati/TipoDanno.java
+++ src/main/java/com/threeamigos/foresta/motore/modellodati/TipoDanno.java
@@ -10,55 +10,61 @@ import com.threeamigos.foresta.motore.Logger;
 public enum TipoDanno {
 
     // Impatti da armi smussate (martelli, mazze) o cadute. Efficace contro scheletri e armature pesanti.
-    CONTUNDENTE(SupertipoDanno.FISICO, TipoEffettoDiStato.STORDITO, TipoEffettoDiStato.ATTERRATO), // Bludgeoning
+    CONTUNDENTE(SupertipoDanno.FISICO, "Contundente", TipoEffettoDiStato.STORDITO, TipoEffettoDiStato.ATTERRATO), // Bludgeoning
     // Attacchi di punta (frecce, lance, morsi). Penetra i punti deboli delle corazze.
-    PERFORANTE(SupertipoDanno.FISICO), // Piercing
+    PERFORANTE(SupertipoDanno.FISICO, "Perforante"), // Piercing
     // Tagli netti (spade, asce, artigli). Causa ferite esposte e sanguinamenti (DoT).
-    TAGLIENTE(SupertipoDanno.FISICO, TipoEffettoDiStato.SANGUINAMENTO), // Slashing
+    TAGLIENTE(SupertipoDanno.FISICO, "Tagliente", TipoEffettoDiStato.SANGUINAMENTO), // Slashing
 
     // Spinte d'aria, lacerazioni da vuoto, tempeste e sbalzi di pressione. Spesso sposta o destabilizza i bersagli.
-    ARIA(SupertipoDanno.ELEMENTALE, TipoEffettoDiStato.ATTERRATO), // Wind/Air
+    ARIA(SupertipoDanno.ELEMENTALE, "Aria", TipoEffettoDiStato.ATTERRATO), // Wind/Air
     // Impatti idrici, annegamento, pressione idrostatica o getti corrosivi. Spesso spegne il fuoco o spinge i nemici.
-    ACQUA(SupertipoDanno.ELEMENTALE, TipoEffettoDiStato.BAGNATO), // Water
+    ACQUA(SupertipoDanno.ELEMENTALE, "Acqua", TipoEffettoDiStato.BAGNATO), // Water
     // Impatti da rocce, terremoti o sabbia. Perfetto per logorare le difese o accecare il bersaglio.
-    TERRA(SupertipoDanno.ELEMENTALE, TipoEffettoDiStato.IMMOBILIZZATO, TipoEffettoDiStato.ACCECATO), // Earth
+    TERRA(SupertipoDanno.ELEMENTALE, "Terra", TipoEffettoDiStato.IMMOBILIZZATO, TipoEffettoDiStato.ACCECATO), // Earth
     // Brucia i bersagli, infiamma gli oggetti e spesso infligge danni nel tempo (DoT).
-    FUOCO(SupertipoDanno.ELEMENTALE, TipoEffettoDiStato.BRUCIATO), // Fire
+    FUOCO(SupertipoDanno.ELEMENTALE, "Fuoco", TipoEffettoDiStato.BRUCIATO), // Fire
     // Danno rapido che spesso si propaga tra bersagli vicini, disattiva reazioni o stordisce.
-    FULMINE(SupertipoDanno.ELEMENTALE, TipoEffettoDiStato.STORDITO), // Lightning
+    FULMINE(SupertipoDanno.ELEMENTALE, "Fulmine", TipoEffettoDiStato.STORDITO), // Lightning
     // Congela, congela i fluidi corporei, rallenta i movimenti e riduce la velocità d'attacco.
-    GELO(SupertipoDanno.ELEMENTALE, TipoEffettoDiStato.RALLENTATO, TipoEffettoDiStato.CONGELATO), // Cold/Ice
+    GELO(SupertipoDanno.ELEMENTALE, "Gelo", TipoEffettoDiStato.RALLENTATO, TipoEffettoDiStato.CONGELATO), // Cold/Ice
     // Corrode armature, scioglie la carne e riduce permanentemente le difese della vittima.
-    ACIDO(SupertipoDanno.ELEMENTALE), // Acid
+    ACIDO(SupertipoDanno.ELEMENTALE, "Acido"), // Acid
     // Onde d'urto invisibili, vibrazioni e boati che spingono indietro i nemici o li assordano.
-    SONICO(SupertipoDanno.ELEMENTALE, TipoEffettoDiStato.ASSORDATO), // Thunder/Sonic
+    SONICO(SupertipoDanno.ELEMENTALE, "Sonico", TipoEffettoDiStato.ASSORDATO), // Thunder/Sonic
     // Tossine che debilitano il bersaglio, riducendo le statistiche e consumando la salute lentamente (DoT).
-    VELENO(SupertipoDanno.ELEMENTALE, TipoEffettoDiStato.AVVELENATO), // Poison
+    VELENO(SupertipoDanno.ELEMENTALE, "Veleno", TipoEffettoDiStato.AVVELENATO), // Poison
 
     // Energia della morte e del decadimento. Dissangua la forza vitale e impedisce la cura.
-    NECROTICO(SupertipoDanno.MAGICO, TipoEffettoDiStato.INFETTATO), // Necrotic
+    NECROTICO(SupertipoDanno.MAGICO, "Necrotico", TipoEffettoDiStato.INFETTATO), // Necrotic
     // Energia di luce divina e radiazioni celestiali. Devastante contro non-morti, demoni e oscurità.
-    SACRO(SupertipoDanno.MAGICO, TipoEffettoDiStato.ACCECATO), // Holy/Radiant
+    SACRO(SupertipoDanno.MAGICO, "Sacro", TipoEffettoDiStato.ACCECATO), // Holy/Radiant
     // Attacchi mentali diretti (illusioni, telepatia). Ignora le armature fisiche e causa confusione o shock.
-    PSICHICO(SupertipoDanno.MAGICO, TipoEffettoDiStato.CONFUSO, TipoEffettoDiStato.SPAVENTATO), // Psychic
+    PSICHICO(SupertipoDanno.MAGICO, "Psichico", TipoEffettoDiStato.CONFUSO, TipoEffettoDiStato.SPAVENTATO), // Psychic
     // Energia magica pura, grezza e manipolata (es. Dardo Incantato). Il tipo di danno meno resistito.
-    ARCANO(SupertipoDanno.MAGICO, TipoEffettoDiStato.SILENZIATO), // Arcane
+    ARCANO(SupertipoDanno.MAGICO, "Arcano", TipoEffettoDiStato.SILENZIATO), // Arcane
     // L'assenza di tutto, la gravità o l'energia delle stelle. Astrazione spaziale che disintegra la materia.
-    VUOTO(SupertipoDanno.MAGICO), // Void/Cosmic
+    VUOTO(SupertipoDanno.MAGICO, "Vuoto"), // Void/Cosmic
     // Corruzione e influssi maligni. Può ridurre i punti vita o intaccare le statistiche massime dei personaggi.
-    MALEDIZIONE(SupertipoDanno.MAGICO), // Curse
+    MALEDIZIONE(SupertipoDanno.MAGICO, "Maledizione"), // Curse
     ;
 
     private final SupertipoDanno superTipo;
+    private final String nome;
     private final TipoEffettoDiStato[] effetti;
 
-    TipoDanno(SupertipoDanno superTipo, TipoEffettoDiStato... effetti) {
+    TipoDanno(SupertipoDanno superTipo, String nome, TipoEffettoDiStato... effetti) {
         this.superTipo = superTipo;
+        this.nome = nome;
         this.effetti = effetti;
     }
 
     public SupertipoDanno getSuperTipo() {
         return superTipo;
+    }
+
+    public String getNome() {
+        return nome;
     }
 
     public TipoEffettoDiStato[] getEffetti() {
