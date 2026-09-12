7124b6adcf082079bac47a10adcfa3357606c7f5
Stefano Reksten <stefano.reksten@gmail.com>
2026-09-12 15:59:49 +0200

    Aggiunto UUID a EventoBase

--- src/main/java/com/threeamigos/foresta/eventi/EventoBase.java
+++ src/main/java/com/threeamigos/foresta/eventi/EventoBase.java
@@ -1,15 +1,22 @@
 package com.threeamigos.foresta.eventi;
 
+import java.util.UUID;
+
 /**
  *
  * @author Stefano Reksten
  */
 public class EventoBase {
 
+    private final String uuid = UUID.randomUUID().toString();
     private final TipoEvento tipoEvento;
 
     protected EventoBase(TipoEvento tipoEvento) {
         this.tipoEvento = tipoEvento;
+    }
+
+    public String getUuid() {
+        return uuid;
     }
 
     public TipoEvento getTipoEvento() {
